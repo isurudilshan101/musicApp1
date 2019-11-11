250c6eab8e796ecac6305b2e7bdc522c7f167b8a
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-11-12 02:32:16 +0530

    comments 9

--- musicApp/app.js
+++ musicApp/app.js
@@ -1,6 +1,15 @@
 window.addEventListener("load", ()=>
 { const sounds=document.querySelectorAll(".sound");
 const pads=document.querySelectorAll(".pads div");
+const visual=document.querySelector('.visual');
+const colors=["#60d394",
+              "#d36060",
+              "#c060d3",
+              "#d3d160",
+              "#60d394",
+              "#60b2d3"
+
+             ];
   
 
 
@@ -8,11 +17,27 @@ const pads=document.querySelectorAll(".pads div");
 
 
 pads.forEach((pad,index) => {
-    pad.addEventListener("click",function(){sounds[index].play();
+    pad.addEventListener("click",function(){
+        
+        sounds[index].currentTime=0; 
+        sounds[index].play();
+        createBubbles(index);
     
      });
   });
+  //create function make a bubbels
 
+  const createBubbles=(index)=>{
+
+    const bubble= document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor=colors[index];
+    bubble.style.animation="jump 1s ease";
+    bubble.addEventListener('animationed',function(){
+        visual.removeChild(this);
+    });
+
+  };
 });
 
 
